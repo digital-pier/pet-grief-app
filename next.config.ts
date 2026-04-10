a1e6400d16416eae6e4ced398305e1d6b4afff47
rick lowe <ralllen7@protonmail.com>
2026-04-10 16:14:24 -0400

    chagned outpu to standalone

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "@prisma/adapter-pg", "pg", "prisma"],
+  output:'standalone'
 };
+
+
 
 export default nextConfig;
